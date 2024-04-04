4f1718aba9381d0446f0e3c4c69da6a7f41e2e8a
Kaithlyn van Deelen <kaithlyn.vdeelen@gmail.com>
2024-04-04 13:27:11 +0200

    Player To Throw has been set (not movement)

--- PvB Game/Assets/Scripts/Player/PlayerMovement.cs
+++ PvB Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	public float force = 2;
+	public float jumpForce = 5f;
 	public int playerIndex;
 	public Rigidbody rb;
 	public float horizontal;
 	public float vertical;
 	public Vector3 playerVelocity;
+	public GameObject playerToThrow;
 	//string[] players = { "PlayerOne", "PlayerTwo", "PlayerThree", "PlayerFour" };
 	// Start is called before the first frame update
 	void Start()
@@ -32,15 +34,59 @@ public class PlayerMovement : MonoBehaviour
 	{
 		if (playerIndex == 0 || playerIndex == 2)
 		{
-			horizontal = Input.GetAxis("HorizontalOne");
-			vertical = Input.GetAxis("VerticalOne");
+			//horizontal = Input.GetAxis("HorizontalOne");
+			//vertical = Input.GetAxis("VerticalOne");
+
+			if (Input.GetKey(KeyCode.W))
+			{
+				transform.position += jumpForce * Time.deltaTime * Vector3.up;
+			}
+			if (Input.GetKey(KeyCode.S))
+			{
+				transform.position += force * Time.deltaTime * Vector3.down;
+			}
+			if (Input.GetKey(KeyCode.A))
+			{
+				transform.position += force * Time.deltaTime * Vector3.right;
+			}
+			if (Input.GetKey(KeyCode.D))
+			{
+				transform.position += force * Time.deltaTime * Vector3.left;
+			}
 		}
 		if (playerIndex == 1 || playerIndex == 3)
 		{
-			horizontal = Input.GetAxis("HorizontalTwo");
-			vertical = Input.GetAxis("VerticalTwo");
+			//horizontal = Input.GetAxis("HorizontalTwo");
+			//vertical = Input.GetAxis("VerticalTwo");
+
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				transform.position += jumpForce * Time.deltaTime * Vector3.up;
+			}
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				transform.position += force * Time.deltaTime * Vector3.down;
+			}
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				transform.position += force * Time.deltaTime * Vector3.right;
+			}
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				transform.position += force * Time.deltaTime * Vector3.left;
+			}
 		}
 
-
+		//playerVelocity = new(horizontal, 0, vertical);
+		//playerVelocity = playerVelocity.normalized * force * Time.deltaTime;
+		//rb.MovePosition(rb.transform.position + playerVelocity);
+	}
+	public void ThrowingGame()
+	{
+		
+	}
+	public void ThrowPlayer()
+	{
+		
 	}
 }
