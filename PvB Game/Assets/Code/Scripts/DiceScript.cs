ee90eb996b3bfcd4ea7414d200dad655cc4d5735
Stormoviek <stormhenkdehaan@gmail.com>
2024-04-04 14:06:46 +0200

    prefabs and better dice velocity

--- PvB Game/Assets/Code/Scripts/DiceScript.cs
+++ PvB Game/Assets/Code/Scripts/DiceScript.cs
@@ -23,13 +23,14 @@ public class DiceScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            rb.velocity = Vector3.zero;
             rollsDone = false;
             float dirX = Random.Range(0, 500);
             float dirY = Random.Range(0, 500);
             float dirZ = Random.Range(0, 500);
             transform.position = new Vector3(0, 2, 0);
             transform.rotation = Quaternion.identity;
-            rb.AddForce(transform.up * 500);
+            rb.AddForce(transform.up * 1000);
             rb.AddTorque(dirX, dirY, dirZ);
         }
     }
